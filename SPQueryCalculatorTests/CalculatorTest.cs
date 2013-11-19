4b788883a77a53acc225c470abc83fdcee377697
ibaiimaz <ibaiimaz@gmail.com>
2013-11-19 09:22:11 +0100

    First version
    Allows to create CAML query with diferent parameters. Only accepts the
    operator "AND" and always the comparision is with "equeal".
    
    You can define the data type but not validate whether the data type is a
    valid one.

---
+++ SPQueryCalculatorTests/CalculatorTest.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SPQueryGenerator;
+
+namespace SPQueryCalculatorTests
+{
+
+    [TestClass]
+    public class CalculatorTest
+    {
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Add_OneTextParam_ReturnsQuery()
+        {
+            Generator calculator = new Generator();
+            string query = @"<Where><Eq><FieldRef Name='field' /><Value Type='Text'>value</Value></Eq></Where>";
+
+            calculator.AddParam("field", "value");
+
+            Assert.AreEqual(query, calculator.Query);
+        }
+
+        [TestMethod]
+        public void Add_AnotherTextParam_ReturnsQuery()
+        {
+            Generator generator = new Generator();
+            string query = @"<Where><Eq><FieldRef Name='field2' /><Value Type='Text'>value2</Value></Eq></Where>";
+
+            generator.AddParam("field2", "value2");
+
+            Assert.AreEqual(query, generator.Query);
+        }
+
+        [TestMethod]
+        public void Add_TwoTextParams_ReturnsQuery()
+        {
+            Generator generator = new Generator();
+            string query = @"<Where><And><Eq><FieldRef Name='field1' /><Value Type='Text'>value1</Value></Eq><Eq><FieldRef Name='field2' /><Value Type='Text'>value2</Value></Eq></And></Where>";
+
+            generator.AddParam("field1", "value1");
+            generator.AddParam("field2", "value2");
+            Assert.AreEqual(query, generator.Query);
+
+        }
+
+        [TestMethod]
+        public void Add_ThreeTextParams_ReturnsQuery()
+        {
+            Generator generator = new Generator();
+            string query = @"<Where><And><And><Eq><FieldRef Name='field1' /><Value Type='Text'>value1</Value></Eq><Eq><FieldRef Name='field2' /><Value Type='Text'>value2</Value></Eq></And><Eq><FieldRef Name='field3' /><Value Type='Text'>value3</Value></Eq></And></Where>";
+
+            generator.AddParam("field1", "value1");
+            generator.AddParam("field2", "value2");
+            generator.AddParam("field3", "value3");
+            Assert.AreEqual(query, generator.Query);
+
+        }
+
+        [TestMethod]
+        public void Add_FourTextParams_ReturnsQuery()
+        {
+            Generator generator = new Generator();
+            string query = @"<Where><And><And><And><Eq><FieldRef Name='field1' /><Value Type='Text'>value1</Value></Eq><Eq><FieldRef Name='field2' /><Value Type='Text'>value2</Value></Eq></And><Eq><FieldRef Name='field3' /><Value Type='Text'>value3</Value></Eq></And><Eq><FieldRef Name='field4' /><Value Type='Text'>value4</Value></Eq></And></Where>";
+
+            generator.AddParam("field1", "value1");
+            generator.AddParam("field2", "value2");
+            generator.AddParam("field3", "value3");
+            generator.AddParam("field4", "value4");
+            Assert.AreEqual(query, generator.Query);
+
+        }
+
+        [TestMethod]
+        public void Add_OneNumberParam_ReturnsTypedQuery()
+        {
+            Generator calculator = new Generator();
+            string query = @"<Where><Eq><FieldRef Name='field' /><Value Type='Number'>value</Value></Eq></Where>";
+
+            calculator.AddParam("field", "value", "Number");
+
+            Assert.AreEqual(query, calculator.Query);
+        }
+
+        [TestMethod]
+        public void Add_TwoNumberParam_ReturnsTypedQuery()
+        {
+            Generator calculator = new Generator();
+            string query = @"<Where><And><Eq><FieldRef Name='field1' /><Value Type='Number'>value1</Value></Eq><Eq><FieldRef Name='field2' /><Value Type='Number'>value2</Value></Eq></And></Where>";
+
+            calculator.AddParam("field1", "value1", "Number");
+            calculator.AddParam("field2", "value2", "Number");
+
+            Assert.AreEqual(query, calculator.Query);
+        }
+    }
+}
---
+++ SPQueryGenerator/Generator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SPQueryGenerator
+{
+    public class Generator
+    {
+        const int WHERE_OPEN_TAG_LENGTH = 7;
+        const int WHERE_OPEN_CLOSE_TAG_LENGTH = 15;
+
+        private string query = "";
+
+        public string Query
+        {
+            get { return query; }
+        }
+
+        public void AddParam(string field, string value)
+        {
+            AddParam(field, value, "Text");
+        }
+
+        public void AddParam(string field, string value, string type)
+        {
+            if (IsFirstParam())
+                AddParamToQuery(field, value, type);   
+            else
+                ConcatParamToQuery(field, value, type);
+        }
+
+        private void ConcatParamToQuery(string field, string value, string type)
+        {
+            int length = query.Length;
+            string beforeQueryWithoutWhereTag = query.Substring(WHERE_OPEN_TAG_LENGTH, length - WHERE_OPEN_CLOSE_TAG_LENGTH);
+            query = "<Where><And>" + beforeQueryWithoutWhereTag + FieldQuerySection(field, value, type) + "</And></Where>";
+        }
+
+        private void AddParamToQuery(string field, string value, string type)
+        {
+            query = "<Where>" + FieldQuerySection(field, value, type) + "</Where>";
+        }
+
+        private string FieldQuerySection(string field, string value, string type)
+        {
+            return string.Format(@"<Eq><FieldRef Name='{0}' /><Value Type='{1}'>{2}</Value></Eq>", field, type, value);
+        }
+
+        private bool IsFirstParam()
+        {
+            return string.IsNullOrEmpty(query);
+        }
+    }
+}
